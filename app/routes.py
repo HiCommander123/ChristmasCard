bd63d024d1b8f6cc3f104d4916c9a7048846127c
lilynim98 <mariokjw@naver.com>
2024-12-07 11:31:03 +0900

    삭제 기능 추가

--- app/routes.py
+++ app/routes.py
@@ -33,3 +33,19 @@ def write_letter():
 def view_letter(letter_id):
     letter = Letter.query.get_or_404(letter_id)
     return render_template('view_letter.html', letter=letter)
+
+@app.route('/delete_letter/<int:letter_id>', methods=['POST'])
+def delete_letter(letter_id):
+    letter = Letter.query.get_or_404(letter_id)
+    
+    # 이미지 파일 삭제 (선택사항)
+    if letter.image:
+        image_path = os.path.join(app.config['UPLOAD_FOLDER'], letter.image)
+        if os.path.exists(image_path):
+            os.remove(image_path)
+    
+    # 데이터베이스에서 편지 삭제
+    db.session.delete(letter)
+    db.session.commit()
+    
+    return redirect(url_for('index'))
